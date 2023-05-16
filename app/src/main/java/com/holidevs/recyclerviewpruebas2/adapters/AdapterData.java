4abd5bad29cb42c5fdcfb40ca886490962bcd503
Roberto Vetere <Roberto.vetere@gmail.com>
2023-05-16 13:42:12 +0200

    Agregando nuevos archivos y modificando AdapterData

--- app/src/main/java/com/holidevs/recyclerviewpruebas2/adapters/AdapterData.java
+++ app/src/main/java/com/holidevs/recyclerviewpruebas2/adapters/AdapterData.java
@@ -1,101 +1,123 @@
-package com.holidevs.recyclerviewpruebas2.adapters;
+package com.holidevs.recyclerviewpruebas2.activities;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.holidevs.recyclerviewpruebas2.R;
-import com.holidevs.recyclerviewpruebas2.activities.MainActivity;
 import com.holidevs.recyclerviewpruebas2.models.Task;
+import com.holidevs.recyclerviewpruebas2.adapters.AdapterData;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
-public class AdapterData extends RecyclerView.Adapter<AdapterData.ViewHolderTask> {
-
-    private ArrayList<Task> listDatos;
-
-    private MainActivity mainActivity;
-
-
-    public AdapterData(ArrayList<Task> listDatos, AdapterData adapterData) {
-        this.listDatos = listDatos;
-    }
-
-    public AdapterData(ArrayList<Task> listDatos, MainActivity mainActivity) {
-        this.listDatos = listDatos;
-        this.mainActivity = mainActivity;
-    }
-
-    @NonNull
-    @Override
-    public ViewHolderTask onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, null, false);
-        return new ViewHolderTask(view);
-
-
-    }
+public class MainActivity extends AppCompatActivity {
+    private List<Task> listDatos;
+    private Button btn_add;
+    private Button btn_all_tasks;
+    private Button btn_personal;
+    private Button btn_professional;
+    private AdapterData taskAdapter;
+    private List<Task> filteredTasks;
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderTask holder, int position) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        AdapterData adapter = new AdapterData(listDatos, this);
+        sharedPreferencesLoad();
 
-        String title = listDatos.get(position).getTitle();
-        String date = listDatos.get(position).getDate();
-        Boolean isCheck = listDatos.get(position).getCheck();
-        holder.title.setText(title);
-        holder.date.setText(date);
-        holder.checkTask.setChecked(isCheck);
+        itemAnimator();
+        initComponents();
+        initAdapterOnMain();
+        setListeners();
+        setupRecyclerView();
+    }
 
+    public void sharedPreferencesLoad() {
+        // Inicializar SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("SharedPreferences", MODE_PRIVATE);
 
-        holder.btnDeleteTask.setOnClickListener(view -> {
+        // Cargar las tareas guardadas en SharedPreferences
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("tasks", "");
+        Type type = new TypeToken<List<Task>>() {}.getType();
+        List<Task> tasks = gson.fromJson(json, type);
 
-            listDatos.remove(holder.getAdapterPosition());
-            notifyItemRemoved(holder.getAdapterPosition());
+        // Inicializar la lista listDatos con los datos cargados
+        listDatos = tasks != null ? tasks : new ArrayList<>();
+    }
 
-            mainActivity.saveSharedPreferences();
-            mainActivity.sharedPreferencesLoad(); // Llama al método sharedPreferencesLoad() de MainActivity
+    private void itemAnimator() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerID);
+        RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
+        itemAnimator.setRemoveDuration(150);
+        recyclerView.setItemAnimator(itemAnimator);
+    }
 
+    private void initComponents() {
+        filteredTasks = new ArrayList<>(listDatos);
+        btn_add = findViewById(R.id.btn_add);
+        btn_all_tasks = findViewById(R.id.btn_allTasks);
+        btn_personal = findViewById(R.id.btn_personal);
+        btn_professional = findViewById(R.id.btn_professional);
+    }
+
+    private void initAdapterOnMain() {
+        RecyclerView recycler = findViewById(R.id.recyclerID);
+        recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        taskAdapter = new AdapterData(listDatos, this); // Inicializar el adaptador
+        recycler.setAdapter(taskAdapter);
+    }
+
+    private void setListeners() {
+        btn_add.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SetNewTaskActivity.class);
+            startActivityForResult(intent, 1);
         });
 
+        btn_all_tasks.setOnClickListener(v -> {
+            showAllTasks();
+        });
 
-        holder.checkTask.setOnCheckedChangeListener((compoundButton, isChecked) -> {
-            Task task = listDatos.get(holder.getAdapterPosition());
-            task.setCheck(isChecked);
-            if (isChecked) {
-                Toast.makeText(holder.itemView.getContext(), "Tarea finalizada: " + task.getTitle(), Toast.LENGTH_SHORT).show();
-                mainActivity.saveSharedPreferences();
-                mainActivity.sharedPreferencesLoad();
+        btn_personal.setOnClickListener(v -> {
+            filteredTasks = filterTasksByCategory("Personal");
+            taskAdapter.updateTasks(filteredTasks);
+        });
+
+        btn_professional.setOnClickListener(v -> {
+            filteredTasks = filterTasksByCategory("Profesional");
+            taskAdapter.updateTasks(filteredTasks);
+        });
+    }
+
+    private void showAllTasks() {
+        filteredTasks = new ArrayList<>(listDatos);
+        taskAdapter.updateTasks(filteredTasks);
+    }
+
+    private List<Task> filterTasksByCategory(String category) {
+        List<Task> filteredTasks = new ArrayList<>();
+
+        for (Task task : listDatos) {
+            if (task != null && task.getType() != null && task.getType().equalsIgnoreCase(category) && !task.isDeleted()) {
+                filteredTasks.add(task);
             }
-        });
+        }
 
+        return filteredTasks;
     }
 
     @Override
-    public int getItemCount() {
-        return listDatos.size();
-    }
-
-    public static class ViewHolderTask extends RecyclerView.ViewHolder {
-
-        private TextView title;
-        private TextView date;
-        private Button btnDeleteTask;
-        private CheckBox checkTask;
-
-        public ViewHolderTask(@NonNull View itemView) {
-            super(itemView);
-            title = itemView.findViewById(R.id.txt_task);
-            date = itemView.findViewById(R.id.txt_date);
-            btnDeleteTask = itemView.findViewById(R.id.btn_delete);
-            checkTask = itemView.findViewById(R.id.checkboxTask);
-        }
-    }
-}
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
